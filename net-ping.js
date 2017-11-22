cb919d71a74ef2cac734f4c7e8ae1dfe9b0760a2
Anthony M Charlton <anthonymcharlton@Anthonys-MacBook-Pro-4.local>
2017-11-22 12:16:18 +0200

    first ping

--- net-ping.js
+++ net-ping.js
@@ -3,6 +3,7 @@ var ping = require ("net-ping");
 var pg = require('pg');
 var session = ping.createSession ();
 var target = '127.0.0.1';
+
 session.pingHost (target, function (error, target, sent, rcvd) {
     var ms = rcvd - sent;
     if (error)
@@ -30,12 +31,14 @@ function insert(ip,val){
                         console.log(err);
                         return res.status(500).json({success: false, data: err});
                     }
-		    var sql = 'INSERT INTO icmp  (timestamp,latency,ip) values (now(),' + val + ',' + ip + ');'
-                    client.query('INSERT INTO icmp ' + sql + ', function (err,result) {
+		    var sql = 'INSERT INTO icmp  (timestamp,latency,ip) values (now(),' + val + ',\'' + ip + '\') returning id;';
+		    console.log(sql);
+                    client.query(sql, function (err,result) {
                         if(err){
                                 console.log("err", err.stack);
                         }else{
                                 done();
+				console.log(result.rows[0]);
                                 //poll(result.rows[0]);
                          }
                     });
